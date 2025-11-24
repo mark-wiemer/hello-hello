33e7fae0a011feee32e68e3d18b6d8a157989214
Mark Wiemer <markwiemer@outlook.com>
2025-11-23 20:52:42 -0800

    Add show AST example

--- packages/remark/index.js
+++ packages/remark/index.js
@@ -1,10 +1,10 @@
 import { unified } from "unified";
 import remarkParse from "remark-parse";
 import remarkStringify from "remark-stringify";
-import { read, write } from "to-vfile";
+import { write } from "to-vfile";
 import { visit } from "unist-util-visit";
 
-// Custom plugin to modify heading levels
+// #region Basic processing pipeline with plugin
 function adjustHeadings() {
   return (tree) => {
     visit(tree, "heading", (node) => {
@@ -12,12 +12,17 @@ function adjustHeadings() {
     });
   };
 }
-
-// Basic processing pipeline
 const file = await unified()
   .use(remarkParse) // Parse Markdown to AST
   .use(adjustHeadings)
   .use(remarkStringify) // Stringify AST back to Markdown
-  .process(await read("input.md"));
-
+  .process("# Hello\n\n- li one\n* li two");
 await write({ path: "output.md", value: String(file) });
+// #endregion
+
+// #region Show full AST
+const processor = unified().use(remarkParse);
+const tree = processor.parse("# Hello\n\n- li one\n* li two");
+
+// console.log(JSON.stringify(tree, null, 2));
+// #endregion Show full AST
