34d09daf712c8d0cbc0bcfde611a6b04a14ced77
Mark Wiemer <markwiemer@outlook.com>
2025-12-31 21:51:12 -0800

    chore(melvor): hide via JS, not CSS
    allows for end-user configuration later

--- packages/melvor/cleanup/menu/src/setup.mjs
+++ packages/melvor/cleanup/menu/src/setup.mjs
@@ -1,4 +1,20 @@
 export function setup(ctx) {
   console.log(`Hello From My Mod ${ctx.version}!`);
   console.log(ctx);
+  const heroStatic = document.querySelector(".hero-static");
+  if (heroStatic) {
+    // hide "A game by Malcs" text
+    hide(heroStatic.querySelector("div:nth-of-type(1) > div > h5:nth-of-type(1)"));
+  }
+}
+
+/**
+ * Sets the element's display to none.
+ *
+ * Does nothing if the element is falsy.
+ * @param {Element} el
+ */
+function hide(el) {
+  if (!el) return;
+  el.style.display = "none";
 }
