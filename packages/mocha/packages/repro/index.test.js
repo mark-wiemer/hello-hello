ece8d91e28c04a54ef620eae6746b579265f40fd
Mark Wiemer <markwiemer@outlook.com>
2026-02-21 06:57:06 -0800

    chore(mocha): simplify repro

--- packages/mocha/packages/repro/index.test.js
+++ packages/mocha/packages/repro/index.test.js
@@ -1,14 +1,5 @@
-var Promise = require("bluebird");
-
-Promise.config({
-  warnings: true,
-  longStackTraces: true,
-});
-
-describe("Suite", function () {
-  it("Test", function () {
-    return Promise.bind(this).then(function () {
-      this.skip();
-    });
+describe("the module", function () {
+  it("works", function () {
+    return 0;
   });
 });
