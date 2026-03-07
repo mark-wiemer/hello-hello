eaccbcfb1de3085440f61b5cfe182cbd35c8cac2
Mark Wiemer <markwiemer@outlook.com>
2026-03-07 07:28:22 -0800

    Remove some markwiemer.com todos, fix typo, update CSpell

--- packages/markwiemer.com/src/utils/recent-posts.ts
+++ packages/markwiemer.com/src/utils/recent-posts.ts
@@ -17,7 +17,6 @@ interface BlogPost {
   };
 }
 
-// todo use in `pages/blog/content/index.astro`
 export function getBlogPostPaths(): BlogPost[] {
   // path: e.g. '/src/pages/blog/content/2024-10-08 months-without-music.mdx'
   // module: object representing the module at that location
