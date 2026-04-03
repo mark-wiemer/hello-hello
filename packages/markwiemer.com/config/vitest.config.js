efb3c4a65646477dfcb68cf549a2371b5be5bf56
Mark Wiemer <markwiemer@outlook.com>
2026-04-03 08:49:14 -0700

    Add MDX stub to get Vitest tests passing

--- packages/markwiemer.com/config/vitest.config.js
+++ packages/markwiemer.com/config/vitest.config.js
@@ -2,7 +2,18 @@
 /// <reference types="vitest" />
 import { getViteConfig } from "astro/config";
 
+/** Stub plugin so Vitest doesn't try to parse MDX as plain JS */
+const mdxStub = {
+  name: "mdx-stub",
+  transform(_code, id) {
+    if (id.endsWith(".mdx")) {
+      return { code: "export default {};", map: null };
+    }
+  },
+};
+
 export default getViteConfig({
+  plugins: [mdxStub],
   // https://vitest.dev/config/
   test: {
     exclude: ["./src/e2e/**", "**/node_modules/**", "**/.git/**"],
