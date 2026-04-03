22c2a0f6e5961e5cb02cb0da4e01f23b2eb14607
Mark Wiemer <markwiemer@outlook.com>
2026-04-03 12:14:49 -0700

    Add test for horizontal scrollbar

--- packages/markwiemer.com/src/e2e/luanti-table.spec.ts
+++ packages/markwiemer.com/src/e2e/luanti-table.spec.ts
@@ -34,4 +34,30 @@ test.describe("luanti people table", () => {
     });
     expect(padding).toBeGreaterThan(0);
   });
+
+  test("page has no horizontal scrollbar @headed", async ({ page }) => {
+    const viewportWidths = [320, 1920];
+
+    // Add random widths between 320 and 1920
+    for (let i = 0; i < 15; i++) {
+      viewportWidths.push(Math.floor(Math.random() * (1920 - 320) + 320));
+    }
+
+    for (const width of viewportWidths) {
+      await page.setViewportSize({ width, height: 1080 });
+      const hasHorizontalScroll = await page.evaluate(() => {
+        return document.documentElement.scrollWidth > document.documentElement.clientWidth;
+      });
+      expect.soft(hasHorizontalScroll, `${width}`).toBe(false);
+    }
+
+    /*
+    We can confirm this test works by adding this rule to see it fail:
+    body {
+      @media (max-width: 480px) {
+        width: 2000px;
+      }
+    }
+    */
+  });
 });
