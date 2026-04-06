71c8825f606083bd01ff364e12c5faca9ea29e9b
Mark Wiemer <markwiemer@outlook.com>
2026-04-05 18:38:26 -0700

    Move markwiemer.com public to src/public...
    ...also update Mocha submodule

--- packages/markwiemer.com/config/astro.config.mjs
+++ packages/markwiemer.com/config/astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import { resolve, sep } from "node:path";
+
+const publicDirPath = `${resolve(process.cwd(), `src${sep}public`)}${sep}`;
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
+  publicDir: "./src/public",
   redirects: {
     "/ahkpp": "https://ahkpp.com",
     "/luanti": "/software/luanti",
@@ -17,7 +21,8 @@ export default defineConfig({
         handleHotUpdate({ file, server }) {
           // public/ assets are served as-is and aren't in Vite's module graph,
           // so changes may not trigger HMR/reload by default.
-          if (file.endsWith("/public/styles.css")) {
+          const normalizedFilePath = resolve(file);
+          if (normalizedFilePath.startsWith(publicDirPath)) {
             server.ws.send({ type: "full-reload", path: "*" });
           }
         },
