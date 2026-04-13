e0334ed0bcf2f4e2007722a0c21d8616bfd2f47a
Mark Wiemer <markwiemer@outlook.com>
2026-04-12 20:15:28 -0700

    Sort blog posts at minute-granularity
    What kind of psycho was I, posting two blog posts on the same day??

--- packages/markwiemer.com/src/utils/blog.ts
+++ packages/markwiemer.com/src/utils/blog.ts
@@ -13,8 +13,24 @@ interface BlogPost {
   // This must be named `props` to work with Astro
   props: {
     /** Object representing the module at that location */
-    module: any;
+    module: {
+      default: any; // todo why does Astro need this?
+      frontmatter: PostFrontmatter;
+    };
   };
+}
+
+interface PostFrontmatter {
+  /** ISO date (not time), e.g. `2026-04-12` */
+  postDate: string;
+  /** Local time (note date), e.g. `20:02 PDT` or `09:32 PST`. Only PDT or PST are used. */
+  postTime: string | undefined;
+  postTitle: string;
+  /** ISO date (not time) */
+  lastUpdated?: string;
+  /** Whether to list the post in the index and home page */
+  listed: boolean;
+  postType?: "notes";
 }
 
 /**
@@ -29,8 +45,27 @@ export function getAllBlogPosts(): BlogPost[] {
     /** e.g. 'months-without-music' */
     const slug = filename.split("-").slice(3).join("-");
 
-    return { params: { slug }, props: { module } };
+    // todo learn more about types here
+    return { params: { slug }, props: { module: module as BlogPost["props"]["module"] } };
   });
 
   return paths;
+}
+
+// per JS sort standards:
+// negative if first arg should precede second arg when sorted
+// zero if equal
+// positive if first arg should come after second arg when sorted
+export function sortBlogPostsOldestFirst(a: PostFrontmatter, b: PostFrontmatter): number {
+  return toTimestamp(a) - toTimestamp(b);
+}
+
+// todo ugh working with dates is not fun
+function toDateString(f: PostFrontmatter): string {
+  if (!f.postTime) return f.postDate;
+  return `${f.postDate} ${f.postTime.slice(0, `00:00`.length)}`;
+}
+
+function toTimestamp(f: PostFrontmatter): number {
+  return Date.parse(toDateString(f));
 }
