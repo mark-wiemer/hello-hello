136af8abeaf022b35bf101dd620482da7fdc3e4f
Mark Wiemer <markwiemer@outlook.com>
2026-04-13 18:58:42 -0700

    Clarify PostFrontmatter type

--- packages/markwiemer.com/src/utils/blog.ts
+++ packages/markwiemer.com/src/utils/blog.ts
@@ -21,21 +21,26 @@ interface BlogPost {
 }
 
 interface PostFrontmatter {
-  /** ISO date (not time), e.g. `2026-04-12` */
+  /** Original posted ISO date (not time), e.g. `2026-04-12` */
   postDate: string;
-  /** Local time (note date), e.g. `20:02 PDT` or `09:32 PST`. Only PDT or PST are used. */
+  /** Original posted local time (not date), e.g. `20:02 PDT` or `09:32 PST`. Only PDT or PST are used. */
   postTime: string | undefined;
+  /** Main heading for the article */
   postTitle: string;
   /** ISO date (not time) */
   lastUpdated?: string;
   /** Whether to list the post in the index and home page */
   listed: boolean;
+  /** Medium URL where this was originally posted */
+  original?: string;
+  /**
+   * If not provided, it's implicitly an article.
+   * Only valid post types are article and notes.
+   */
   postType?: "notes";
 }
 
-/**
- * Returns all blog posts.
- */
+/** Returns all blog posts in no particular order */
 export function getAllBlogPosts(): BlogPost[] {
   // path: e.g. '/src/pages/blog/content/2024-10-08-months-without-music.mdx'
   // module: object representing the module at that location
