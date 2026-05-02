93b554dc7d16d75a489b7bf99d9dc8efc4877211
Mark Wiemer <markwiemer@outlook.com>
2026-05-02 09:58:12 -0700

    Remove stale Mocha projects

--- packages/mocha/packages/custom-reporter/add.test.js
+++
@@ -1,8 +0,0 @@
-import { add } from "./add.js";
-import assert from "node:assert";
-
-describe("add", () => {
-  it("works", () => {
-    assert.equal(add(1, 1), 2);
-  });
-});
--- packages/mocha/packages/semver-behavior/main.js
+++
@@ -1,5 +0,0 @@
-// https://github.com/mochajs/mocha/issues/5763
-import { gt } from "semver";
-const vNew = "12.0.0-beta-10.1";
-const vCurrent = "12.0.0-beta-10";
-console.log(`${vNew} > ${vCurrent}`, gt(vNew, vCurrent));
