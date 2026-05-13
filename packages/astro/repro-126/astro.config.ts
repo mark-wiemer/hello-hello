1a418a4e764915759f2fda5cd26dcb93b8771e3a
Mark Wiemer <markwiemer@outlook.com>
2026-05-13 13:03:53 -0700

    Add vite.plugins to astro.config.ts, still no repro
    #126

--- packages/astro/repro-126/astro.config.ts
+++ packages/astro/repro-126/astro.config.ts
@@ -1,5 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import { resolve, sep } from "node:path";
+import process from "node:process";
+
+const publicDirPath = `${resolve(process.cwd(), `src${sep}public`)}${sep}`;
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [
+      {
+        name: "full-reload-public-styles",
+        apply: "serve",
+        handleHotUpdate({ file, server }) {
+          // public/ assets are served as-is and aren't in Vite's module graph,
+          // so changes may not trigger HMR/reload by default.
+          const normalizedFilePath = resolve(file);
+          if (normalizedFilePath.startsWith(publicDirPath)) {
+            server.ws.send({ type: "full-reload", path: "*" });
+          }
+        },
+      },
+    ],
+  },
+});
