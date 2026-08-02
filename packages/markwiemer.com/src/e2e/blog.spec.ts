8594db9f44bfcfd47b6b56a90e5f11a385b97e7c
Mark Wiemer <markwiemer@outlook.com>
2026-08-01 18:51:06 -0700

    Add model-harness-piracy draft

--- packages/markwiemer.com/src/e2e/blog.spec.ts
+++ packages/markwiemer.com/src/e2e/blog.spec.ts
@@ -75,6 +75,7 @@ test.describe("blog index", () => {
       "immigration-to-us",
       "donald-trump",
       "blessing-in-disguise-2026",
+      "model-harness-piracy",
     ].reverse();
 
     const actualListedBlogIds = await getListedPosts(page);
