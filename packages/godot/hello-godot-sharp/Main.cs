ae1111c74a35ed9b997795dcb7679a48004eb091
Mark Wiemer <markwiemer@outlook.com>
2026-08-02 10:35:00 -0700

    Update docs for HGS Main.cs

--- packages/godot/hello-godot-sharp/Main.cs
+++ packages/godot/hello-godot-sharp/Main.cs
@@ -6,46 +6,61 @@ using System.Linq;
 
 public partial class Main : Node
 {
+	#region Globals
 	[Export]
 	public PackedScene SegmentScene;
 
-	// Game variables
+	#region Game variables
 	private int score = 0;
 	private bool gameStarted = false;
+	#endregion Game variables
 
-	// Grid variables
-	// Number of cells in each row and column of the grid
+	#region Grid variables
+	/// <summary>
+	/// Number of cells in each row and column of the grid
+	/// </summary>
 	private readonly int gridSize = 20;
 	// Size of each cell in pixels
 	private readonly int cellSize = 50;
+	#endregion Grid variables
 
-	// Snake variables
+	#region Snake variables
 	private List<Vector2> snakePos;
 	private List<ColorRect> snakeRect;
 
-	// Movement variables
-	private Vector2 startPos = new(9, 9);
-	private Vector2 up = new(0, -1);
-	private Vector2 down = new(0, 1);
-	private Vector2 left = new(-1, 0);
-	private Vector2 right = new(1, 0);
+	/// <summary>Whether the snake can turn this tick</summary>
+	private bool canTurn;
 
 	/// <summary>
 	/// Direction in which the snake is currently moving.
 	/// Starts as zero vector to allow game to start by moving in any direction.
 	/// </summary>
 	private Vector2 snakeDir = new(0, 0);
+	private Vector2 startPos = new(9, 9);
+	#endregion Snake variables
 
-	/// <summary>Whether the snake can turn this tick</summary>
-	private bool canTurn;
+	#region Directions
+	private Vector2 up = new(0, -1);
+	private Vector2 down = new(0, 1);
+	private Vector2 left = new(-1, 0);
+	private Vector2 right = new(1, 0);
+	#endregion Directions
 
-	// Called when the node enters the scene tree for the first time.
+	#endregion Globals
+
+	/// <summary>
+	/// Magic function called when node enters scene tree for the first time.
+	/// Resets game.
+	/// </summary>
 	public override void _Ready()
 	{
-		NewGame();
+		ResetGame();
 	}
 
-	private void NewGame()
+	/// <summary>
+	/// Resets score and regenerate initial snake.
+	/// </summary>
+	private void ResetGame()
 	{
 		score = 0;
 		GetNode("HUD").GetNode<Label>("Score").Text = "Score: " + score;
@@ -53,6 +68,9 @@ public partial class Main : Node
 		GenerateSnake();
 	}
 
+	/// <summary>
+	/// Generates a starting snake, facing up, at the starting pos.
+	/// </summary>
 	private void GenerateSnake()
 	{
 		snakePos = [];
@@ -64,18 +82,21 @@ public partial class Main : Node
 		}
 	}
 
-	private void AddSegment(Vector2 pos)
+	/// <summary>
+	/// Adds a segment to the snake at the given board position.
+	/// </summary>
+	/// <param name="boardPos"></param>
+	private void AddSegment(Vector2 boardPos)
 	{
-		snakePos.Add(pos);
+		snakePos.Add(boardPos);
 		ColorRect Segment = SegmentScene.Instantiate<ColorRect>();
-		// plus one cell height for score panel
-		Segment.Position = ToGamePos(pos);
+		Segment.Position = ToWindowPos(boardPos);
 		AddChild(Segment);
 		snakeRect.Add(Segment);
 	}
 
 	/// <summary>
-	/// Called every frame. Polls for user input.
+	/// Magic function called every frame. Polls for user input.
 	/// </summary>
 	/// <param name="delta">Elapsed time since previous frame</param>
 	public override void _Process(double delta)
@@ -133,6 +154,9 @@ public partial class Main : Node
 		}
 	}
 
+	/// <summary>
+	/// Start moving the snake.
+	/// </summary>
 	private void StartGame()
 	{
 		gameStarted = true;
@@ -150,6 +174,9 @@ public partial class Main : Node
 		canTurn = true;
 	}
 
+	/// <summary>
+	/// Adjust snake's position based on its current direction.
+	/// </summary>
 	private void MoveSnake()
 	{
 		var oldHead = snakePos[0];
@@ -159,12 +186,18 @@ public partial class Main : Node
 		for (int i = 0; i < snakePos.Count; i++)
 		{
 			var pos = snakePos[i];
-			snakeRect[i].Position = ToGamePos(pos);
+			snakeRect[i].Position = ToWindowPos(pos);
 		}
 	}
 
-	private Vector2 ToGamePos(Vector2 pos)
+	/// <summary>
+	/// Convert a board position to a window position
+	/// </summary>
+	/// <param name="boardPos">Position on the board</param>
+	/// <returns>Position, in pixels from top-left of window</returns>
+	private Vector2 ToWindowPos(Vector2 boardPos)
 	{
-		return (pos * cellSize) + new Vector2(0, cellSize);
+		// add one cell height for HUD
+		return (boardPos * cellSize) + new Vector2(0, cellSize);
 	}
 }
