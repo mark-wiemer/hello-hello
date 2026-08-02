015428126343dcb19d05800d6d22b96a2260baa7
Mark Wiemer <markwiemer@outlook.com>
2026-08-01 18:55:56 -0700

    Format Godot Snake Main.cs

--- packages/godot/hello-godot-sharp/Main.cs
+++ packages/godot/hello-godot-sharp/Main.cs
@@ -6,22 +6,22 @@ public partial class Main : Node
 {
 	[Export]
 	public PackedScene SegmentScene;
-	
+
 	// Game variables
 	private int _score = 0;
 	private bool _gameStarted = false;
-	
+
 	// Grid variables
 	// Number of cells in each row and column of the grid
 	private readonly int _gridSize = 20;
 	// Size of each cell in pixels
 	private readonly int _cellSize = 50;
-	
+
 	// Snake variables
 	private List<Vector2> _oldData;
 	private List<Vector2> _newData;
-	private List<ColorRect> _snake; 
-	
+	private List<ColorRect> _snake;
+
 	// Movement variables
 	private Vector2 _startPos = new Vector2(9, 9);
 	private Vector2 _up = new Vector2(0, -1);
@@ -30,14 +30,15 @@ public partial class Main : Node
 	private Vector2 _right = new Vector2(1, 0);
 	private Vector2 _direction;
 	private bool _canMove;
-	
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		NewGame();
 	}
 
-	private void NewGame() {
+	private void NewGame()
+	{
 		_score = 10;
 		GetNode("HUD").GetNode<Label>("Score").Text = "Score: " + _score;
 		_direction = _up;
@@ -45,17 +46,20 @@ public partial class Main : Node
 		GenerateSnake();
 	}
 
-	private void GenerateSnake() {
+	private void GenerateSnake()
+	{
 		_oldData = new();
 		_newData = new();
 		_snake = new();
 
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < 3; i++)
+		{
 			AddSegment(_startPos + new Vector2(0, i));
 		}
 	}
 
-	private void AddSegment(Vector2 pos) {
+	private void AddSegment(Vector2 pos)
+	{
 		_newData.Add(pos);
 		ColorRect Segment = SegmentScene.Instantiate<ColorRect>();
 		// plus room at top for score panel
